9c38655232ad7731445f3158549a59b0291d37c3
Daksh Tyagi <100674078+daksh1402@users.noreply.github.com>
2024-05-16 13:42:13 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Daksh Tyagi",
-  title: "Hi all, I'm Daksh",
+  title: "Hi all, I'm Daksh Tyagi",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
